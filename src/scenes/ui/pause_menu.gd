d1d27f15c4e4322a8213bb00f83d8424fa58128f
Wallabee01 <phrozenfire01@gmail.com>
2025-07-21 09:52:10 -0700

    Final
    Created icon.ico and added Quit button to pause menu

--- src/scenes/ui/pause_menu.gd
+++ src/scenes/ui/pause_menu.gd
@@ -1,6 +1,9 @@
 extends CanvasLayer
 
+@export var start_label: Label
+
 var is_paused: bool = false
+var is_start_label_visible: bool = false
 
 
 func _unhandled_input(event):
@@ -9,8 +12,15 @@ func _unhandled_input(event):
 			get_tree().paused = true
 			is_paused = true
 			visible = true
+			is_start_label_visible = start_label.visible
+			start_label.visible = false
 		else:
 			get_tree().paused = false
 			is_paused = false
 			visible = false
+			start_label.visible = is_start_label_visible
 	
+
+
+func _on_button_pressed():
+	get_tree().quit()
